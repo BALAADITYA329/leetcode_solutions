e6e4b78f19f89f9d7f72bb031fc4c64809298857
BalaAditya <balaaditya329@gmail.com>
2026-09-08 20:34:05 +0530

    Time: 7 ms (53.65%) | Memory: 19.2 MB (75.31%) - LeetSync

---
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -0,0 +1,13 @@
+def self_div_numbers(n):
+    s=str(n)
+    for i in s:
+        if i=='0' or n%int(i)!=0:
+            return False
+    return True
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        li=[]
+        for i in range(left,right+1):
+            if self_div_numbers(i):
+                li.append(i)
+        return li
