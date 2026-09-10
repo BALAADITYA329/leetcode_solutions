5221902b4fd33b8c6436a77f9415b0fee87902fa
BalaAditya <balaaditya329@gmail.com>
2026-09-10 10:34:47 +0530

    Time: 55 ms (39.15%) | Memory: 39.6 MB (48.21%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -2,7 +2,7 @@ class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         dic={0:-1}
         ps=0
-        for i,num in enumerate(nums):
+        for i in range(len(nums)):
             ps+=nums[i]
             r=ps%k
             if r in dic:
